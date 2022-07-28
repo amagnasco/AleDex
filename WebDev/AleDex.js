9ae9e676aff05bd332b2245b786e591130f87536
Alessandro G. Magnasco <alessandromagnasco@gmail.com>
2022-07-28 18:15:58 -0400

    7/28 update

--- WebDev/AleDex.js
+++ WebDev/AleDex.js
@@ -4,49 +4,59 @@
 // PAGE SETUP
 // generate loading messages
 // generate buttons
-// load map from MapBox
-    var map = L.map('map').setView([42, -72], 13);
-
-    let dummyData = [
-        {
-            name: "nyc",
-            lng: -73.96820068359375,
-            lat: 40.772221877329024
-
-        },
-        {
-            name: "la",
-            lng: -118.125,
-            lat: 34.016241889667015
-        }
-    ]
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
-
-    let bndsArray = []
-
-    dummyData.forEach(function (city) {
-        var marker = L.marker([city.lat, city.lng]).addTo(map);
-        bndsArray.push([city.lat, city.lng])
+// estimate location of user
+    /* superseded by mapbox internal geolocation
+    navigator.geolocation.getCurrentPosition(function(location){
+        const user_lat = location.coords.latitude;
+        const user_lng = location.coords.longitude;
+        document.getElementById("user-location").innerText = `Your device is reporting its location as Lat: ${lat}, Lng: ${lng}`
     });
+    */
 
-    map.fitBounds(bndsArray)
+// check if MapBox can render in this browser
+// https://docs.mapbox.com/mapbox-gl-js/example/check-for-support/
 
-    //Create an empty array that will have all the results appended to
-    let allResults = [];
-    //Search radius
-    let radius = 1;
-    //Lat/lng
-    let lat = 40.78885994449482;
-    let lng = -74.1577148;
+// load map from MapBox
+// https://docs.mapbox.com/mapbox-gl-js/example/drag-a-point/
 
-    //What to do when everything is done
-    function processFullResults(){
-        //Add all the results to the map
-    }
+	mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hZ25hc2NvIiwiYSI6ImNsNjVqcTk1YjAzZzkzZHM3OXFoMTJqMzUifQ.O-oSk96QJMtHyMzQL80VyA';
+    const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-24, 42], // starting center in [lng, lat]
+    zoom: 1, // starting zoom
+    projection: 'globe' // display map as a 3D 'globe'. can use 'naturalEarth' for 2d
+    });
+     
+    map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+    });
+     
+    // Add a button for user to geolocate their device
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+        })
+    );
+
+    // Add text input box for user to search for location
+    // https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/ 
+    map.addControl(
+        new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+        })
+    );
+
+    // change map style 
+    // https://docs.mapbox.com/mapbox-gl-js/example/setstyle/
+
 // let user know page is ready to search
 
 // USER INPUT VALIDATION
@@ -56,51 +66,49 @@
 // print valid input as coordinates and accuracy radius
 
 // SEARCH
-//Take the minimum id to start looking for
+// Take the minimum id to start looking for
 function recursiveGet(min_id, totalResults) {
 
     //Construct the url
     let url = `https://api.inaturalist.org/v1/observations?id_above=${min_id}&lat=${lat}&lng=${lng}&radius=${radius}&order=asc&order_by=id&per_page=200`
 
-    //Get the data
+    // Get the data
     axios.get(url).then((rsp) => {
-        //Concat the array returned in results to all results
+        // Concat the array returned in results to all results
         allResults = allResults.concat(rsp.data.results);
         console.log(allResults)
         console.log(rsp.data.results)
-        //Wait so you don't timeout the API
+        // Wait so you don't timeout the API
         setTimeout(() => {
-            //If the length of all the results is still less than the total, increment the minimum id and try again
+            // If the length of all the results is still less than the total, increment the minimum id and try again
             if (allResults.length < totalResults || typeof rsp.data.results === 'undefined') {
                 recursiveGet((rsp.data.results[rsp.data.results.length - 1].id), totalResults);
             }else{
-                //Otherwise, it's done!
+                // Otherwise, it's done!
                 console.log('DONE!')
                 processFullResults();
             }
         }, 100);
-
-
         console.log(url);
         console.log(`${totalResults - allResults.length} more needed!`)
         console.log(allResults)
     })
 }
 
-//Get the total number of results and the id of the first result
+// Get the total number of results and the id of the first result
 function getNumResults() {
     let url = `https://api.inaturalist.org/v1/observations?lat=${lat}&lng=${lng}8&radius=${radius}&order=asc&order_by=id&per_page=1`
     axios.get(url).then((rsp) => {
         let total = rsp.data.total_results;
         console.log(total)
-        //Start off by looking at 1 less than the first id
+        // Start off by looking at 1 less than the first id
         recursiveGet(+rsp.data.results[0].id - 1, total);
-
-
     })
 }
 
 //Start it all off
-getNumResults();
+// getNumResults();
 
-// GENERATE RESULTS
+// GENERATE STATISTICS ON PAGE
+
+// GENERATE RESULTS ON PAGE
