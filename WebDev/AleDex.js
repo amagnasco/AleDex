c16a1501ffbd6b3db185c9f794d68ec50ba2537b
Alessandro G. Magnasco <alessandromagnasco@gmail.com>
2022-08-02 18:35:56 -0400

    8/2 update

--- WebDev/AleDex.js
+++ WebDev/AleDex.js
@@ -4,14 +4,9 @@
 // PAGE SETUP
 // generate loading messages
 // generate buttons
-// estimate location of user
-    /* superseded by mapbox internal geolocation
-    navigator.geolocation.getCurrentPosition(function(location){
-        const user_lat = location.coords.latitude;
-        const user_lng = location.coords.longitude;
-        document.getElementById("user-location").innerText = `Your device is reporting its location as Lat: ${lat}, Lng: ${lng}`
-    });
-    */
+// generate initial longitude and latitude
+    let coords = {lng: -72,lat: 22}
+    let coords_accuracy = 1000; // in meters
 
 // check if MapBox can render in this browser
 // https://docs.mapbox.com/mapbox-gl-js/example/check-for-support/
@@ -20,18 +15,36 @@
 // https://docs.mapbox.com/mapbox-gl-js/example/drag-a-point/
 
 	mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hZ25hc2NvIiwiYSI6ImNsNjVqcTk1YjAzZzkzZHM3OXFoMTJqMzUifQ.O-oSk96QJMtHyMzQL80VyA';
+    
     const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-24, 42], // starting center in [lng, lat]
-    zoom: 1, // starting zoom
-    projection: 'globe' // display map as a 3D 'globe'. can use 'naturalEarth' for 2d
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [coords.lng, coords.lat], // starting center in [lng, lat]
+        zoom: 1, // starting zoom
+        projection: 'globe', // display map as a 3D 'globe'. can use 'naturalEarth' for 2d
+        // add pitch and bearing
+        //pitch: 50,
+        //bearing: 150,
     });
      
     map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
+        map.setFog({}); // Set the default atmosphere style
     });
-     
+
+    //map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+
+    // Add text input box for user to search for location
+    // https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/ 
+    map.addControl(
+        new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+
+        // update lon, lat
+        //coords = 
+        })
+    );
+
     // Add a button for user to geolocate their device
     map.addControl(
         new mapboxgl.GeolocateControl({
@@ -42,17 +55,34 @@
         trackUserLocation: true,
         // Draw an arrow next to the location dot to indicate which direction the device is heading.
         showUserHeading: true
+
+        // update lon, lat
+        // coords  = 
         })
     );
 
-    // Add text input box for user to search for location
-    // https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/ 
-    map.addControl(
-        new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-        })
-    );
+    // Determine clicked location
+    // https://docs.mapbox.com/mapbox-gl-js/example/mouse-position/
+    map.on('click', (e) => {
+        // update lon, lat
+        coords = e.lngLat;
+        // set a marker
+        //marker.setLngLat(coords);
+
+        // Sample & round the terrain elevation
+        let elevation = Math.floor(
+            // Do not use terrain exaggeration to get actual meter values
+            map.queryTerrainElevation(coords, { exaggerated: false })
+        );
+        
+        // display lon, lat, elevation
+        document.getElementById('selected-area').innerHTML = 
+            'Lat: ' + coords.lat + ' '
+            + 'Lon: ' + coords.lng
+            + '<br>'
+            + 'Elevation: ' + elevation + 'm (work in progress); '
+            + 'Accuracy: ' + coords_accuracy + 'm'
+    });
 
     // change map style 
     // https://docs.mapbox.com/mapbox-gl-js/example/setstyle/
